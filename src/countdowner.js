e9eb28c531f285ae9b6505320c45b7da4b22d96d
Lukas Horak <horaklukas@centrum.cz>
2016-03-06 20:47:35 +0100

    First working version, add example file

--- src/countdowner.js
+++ src/countdowner.js
@@ -1,3 +1,41 @@
+// Date.now ponyfill 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+if (!Date.now) {
+  Date.now = function now() {
+    return new Date().getTime();
+  };
+}
+
+// Function.prototype.bind ponyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // Function.prototype doesn't have a prototype property
+      fNOP.prototype = this.prototype; 
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
+
 /**
  * @param {Date} targetDate
  * @return {Object.<string, number>}
@@ -7,8 +45,15 @@ var Countdowner = function(targetDate) {
     throw new Error('Only Date object is accepted as a constructor parameter');
   }
 
-  this.targetDate = targetDate;
-}
+  Countdowner.index++;
+
+  this.ID = 'cd' + Countdowner.index.toString();
+  this.targetDateMs = targetDate.getTime();
+  this.timer = null;
+};
+
+// Index counter for Countdowners, also count of existing instances
+Countdowner.index = 0;
 
 Countdowner.MS_IN_SEC = 1000;
 Countdowner.MS_IN_MIN = Countdowner.MS_IN_SEC * 60;
@@ -18,7 +63,7 @@ Countdowner.MS_IN_DAY = Countdowner.MS_IN_HOUR * 24;
 /**
  * @param {number} miliseconds
  */
-Countdowner.decomposeMiliseconds = function(miliseconds) {
+Countdowner.prototype.decomposeMiliseconds = function(miliseconds) {
   var days, hours, minutes, seconds, rest;
 
   days = Math.floor(miliseconds / Countdowner.MS_IN_DAY);
@@ -38,16 +83,50 @@ Countdowner.decomposeMiliseconds = function(miliseconds) {
     minutes: minutes,
     seconds: seconds
   }
+};
+
+/**
+ * @param {?HTMLElement} placeholder
+ */
+Countdowner.prototype.create = function(placeholder) {
+  if(!placeholder) {
+    placeholder = document.createElement('div');
+    document.body.appendChild(placeholder);
+  }
+
+  // @TODO - if already has id, save it insteaad of create new
+  placeholder.id = this.ID;
+  this.tick();
+};
+
+Countdowner.prototype.tick = function () {
+  var message, timeDiff, decomposedMiliseconds;
+  
+  timeDiff = this.targetDateMs - Date.now();
+
+  if(timeDiff < 0) {
+    message = 'Událost již nastala';
+    this.timer = null;
+  } else {
+    message = this.getCountdownMessage(this.decomposeMiliseconds(timeDiff));
+    this.timer = setTimeout(this.tick.bind(this), Countdowner.MS_IN_SEC);
+  }
+
+  this.display(message);
 }
+
+Countdowner.prototype.display = function(text) {
+    document.getElementById(this.ID).innerHTML = text;
+};
 
 /**
  * @param {Object.<string, number>} remainingTime
  */
 Countdowner.prototype.getCountdownMessage = function(remainingTime) {
-  return remainingTime.days + ' dnů' + remainingTime.hours + ' hodin' +
+  return remainingTime.days + ' dnů ' + remainingTime.hours + ' hodin ' +
     remainingTime.minutes + ' minut a ' + remainingTime.seconds + ' vteřin';
-}
+};
 
-if(module != null) {
+if(typeof module !== "undefined" && module !== null) {
   module.exports = Countdowner;
 }
--- test/countdownerTest.js
+++ test/countdownerTest.js
@@ -8,6 +8,14 @@ describe('Countdowner', function () {
     Countdowner = require('../src/countdowner');
   });
 
+  beforeEach(function () {
+    Countdowner.index = 0;
+  });
+
+  it('should have zero count of countdowners on start', function () {
+    expect(Countdowner.index).toEqual(0);
+  });
+
   describe('#constructor', function () {
     it('should throw when date not supplied to constructor', function () {
       expect(function() {
@@ -21,27 +29,54 @@ describe('Countdowner', function () {
       }).toNotThrow();
     });
 
-    it('should store passed date at instance', function () {
+    it('should store miliseconds of date supplied to instance', function () {
       var date = new Date();
       var cd = new Countdowner(date);
 
-      expect(cd.targetDate.getTime()).toEqual(date.getTime());
-    })
+      expect(cd.targetDateMs).toEqual(date.getTime());
+    });
+
+    it('should increment count of instances', function () {
+      new Countdowner(new Date);
+      new Countdowner(new Date);
+
+      expect(Countdowner.index).toEqual(2);
+    });
+
+    it('should create ID of related element', function() {
+      cd1 = new Countdowner(new Date);
+      cd2 = new Countdowner(new Date);
+      cd3 = new Countdowner(new Date);
+
+      expect(cd1.ID).toEqual('cd1');      
+      expect(cd2.ID).toEqual('cd2');      
+      expect(cd3.ID).toEqual('cd3');      
+    });
   });
 
   describe('#decomposeMiliseconds', function () {
+    before(function () {
+      this.cd = new Countdowner(new Date);
+    });
+
+    after(function () {
+      delete this.cd;
+    });
+
     it('should extract correct count of days', function () {
-      var ms = (1000 * 60 * 60 * 24 * 14);
-      var decomposed = Countdowner.decomposeMiliseconds(ms);
+      // 14 days, 18 hours, 28 minutes 2 seconds
+      var ms = (1000 * 60 * 60 * 24 * 14) + (1000 * 60 * 60 * 18) +
+        (1000 * 60 * 28) + (1000 * 2);
+      var decomposed = this.cd.decomposeMiliseconds(ms);
 
       expect(decomposed.days).toEqual(14);
     });
 
     it('should extract correct count of hours', function () {
-      // 2days, 22 hours, 52 minutes 50 seconds
+      // 2 days, 22 hours, 52 minutes 50 seconds
       var ms = (1000 * 60 * 60 * 24 * 2) + (1000 * 60 * 60 * 22) +
         (1000 * 60 * 52) + (1000 * 50);
-      var decomposed = Countdowner.decomposeMiliseconds(ms);
+      var decomposed = this.cd.decomposeMiliseconds(ms);
 
       expect(decomposed.hours).toEqual(22);
     });
@@ -49,7 +84,7 @@ describe('Countdowner', function () {
     it('should extract correct count of minutes', function () {
       // 18 hours, 31 minutes, 5 seconds
       var ms = (1000 * 60 * 60 * 18) + (1000 * 60 * 31) + (1000 * 5);
-      var decomposed = Countdowner.decomposeMiliseconds(ms);
+      var decomposed = this.cd.decomposeMiliseconds(ms);
 
       expect(decomposed.minutes).toEqual(31);
     });
@@ -57,9 +92,45 @@ describe('Countdowner', function () {
     it('should extract correct count of seconds', function () {
       // 3 minutes, 2 seconds
       var ms = (1000 * 60 * 3) + (1000 * 2);
-      var decomposed = Countdowner.decomposeMiliseconds(ms);
+      var decomposed = this.cd.decomposeMiliseconds(ms);
 
       expect(decomposed.seconds).toEqual(2);
+    });
+  });
+
+  describe('#tick', function () {
+    beforeEach(function () {
+      this.targetDate = new Date(1000 * 25);
+      this.cd = new Countdowner(this.targetDate); 
+      sinon.stub(this.cd, 'getCountdownMessage');
+      sinon.stub(this.cd, 'decomposeMiliseconds');
+      sinon.stub(this.cd, 'display');
+      this.clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function () {
+      this.cd.getCountdownMessage.restore();
+      this.cd.decomposeMiliseconds.restore();      
+      this.cd.display.restore();   
+      this.clock.restore()   ;
+    });
+
+    it('should display "Event passed" message when actual time is greater than target', function () {
+      this.clock.tick(1000 * 26);
+
+      this.cd.tick();
+
+      expect(this.cd.display.calledOnce).toEqual(true);
+    });
+
+    it('should decompose remaining miliseconds each second', function () {
+      var ticks = 18;
+      this.clock.tick(this.targetDate.getTime() - (1000 * ticks));
+      
+      this.cd.tick();
+      this.clock.tick(1000 * ticks);
+      
+      expect(this.cd.decomposeMiliseconds.callCount).toEqual(ticks + 1);
     });
   });
 
